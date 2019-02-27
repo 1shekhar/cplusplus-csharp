64278071ebe1b570de3a2ba337f85be71e847684
Shekhar <chandrashekhar.bhosale@acellere.com>
2019-02-27 18:34:48 +0530

    test

---
+++ cppDll/cppDll/test.h
@@ -0,0 +1,23 @@
+#ifndef __TEST_DLL_H__
+#define __TEST_DLL_H__
+
+#if defined (EXPORTBUILD)  
+# define _DLLExport extern "C" __declspec (dllexport)  
+# else  
+# define _DLLExport extern "C" __declspec (dllimport)  
+#endif  
+
+_DLLExport int add(int x, int y);
+
+typedef struct
+{
+	int a;
+	int b;
+	int c;
+} CppStruct;
+
+_DLLExport bool getStructData(CppStruct& d);
+_DLLExport bool getStructDataPtr(CppStruct* d);
+_DLLExport void setArrayData(int count, CppStruct* d);
+
+#endif
